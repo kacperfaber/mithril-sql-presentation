83995a15572f012964bfc4bc7c2db75a1bfb7d1c
Kacper Faber <kacperf1234@gmail.com>
2023-02-23 16:30:49 +0100

    Added Presentation_Heading with animated button to scroll down. Changed download link to drive.google.com shared resource. Added left side [color success] border to sql console. SqlFetch -> SqlConsole no withResetButton anymore. I finished.

--- src/ui/apiDownload/ApiDownload.tsx
+++ src/ui/apiDownload/ApiDownload.tsx
@@ -6,7 +6,7 @@ export const ApiDownload = function () {
             <>
                 <h1>Download PHP API</h1>
                 <p>Read the docs before you install it...</p>
-                <a className="btn btn-link" href="https://github.com/kacperfaber/mitrhil-sql-presentation/blob/master/api/api.php">Download</a>
+                <a className="btn btn-link" href="https://drive.google.com/drive/folders/12JpRt_BjRs9XEiaMWhrekR8g0IzvRtvj?usp=sharing">Download</a>
                 <a className="btn btn-link" href="https://github.com/kacperfaber/mitrhil-sql-presentation/blob/master/api/api.txt">Documentation</a>
             </>
         )
---
+++ src/ui/presentation/Presentation_Heading.tsx
@@ -0,0 +1,5 @@
+export const Presentation_Heading = function () {
+    return {view: () => (<div className="animate-in" style="width: 100%; height: 100vh; text-align: center;"><h1>Podstawy SQL</h1><p>Prezentacja
+            interaktywna</p><a href="#presentation-container" className="btn btn-primary">START</a></div>)
+    }
+}
